a6010d406af9ea94a56203679ec409f0358390ed
rodchen <rodchen@augmentum.com.cn>
2018-05-08 16:50:55 +0800

    1. react-route

--- react-cli/src/App.js
+++ react-cli/src/App.js
@@ -5,42 +5,19 @@ import About from "./containers/about";
 import Inbox from "./containers/inbox";
 import Home from "./containers/home";
 
+import { Router, Route, Link } from 'react-router'
+
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {route: window.location.hash.substr(1)};
-    }
-
-    componentDidMount() {
-        window.addEventListener('hashchange', () => {
-            this.setState({
-                route: window.location.hash.substr(1)
-            })
-        })
-    }
-
     render() {
-        let Child
-        switch (this.state.route) {
-            case '/about':
-                Child = About;
-                break;
-            case '/inbox':
-                Child = Inbox;
-                break;
-            default:
-                Child = Home;
-        }
-
         return (
             <div>
                 <h1>App</h1>
                 <ul>
-                    <li><a href="#/about">About</a></li>
-                    <li><a href="#/inbox">Inbox</a></li>
-                    <li><a href="#/home">Home</a></li>
+                    <li><link href="#/about">About</link></li>
+                    <li><link href="#/inbox">Inbox</link></li>
+                    <li><link href="#/home">Home</link></li>
                 </ul>
-                <Child/>
+                {this.props.children}
             </div>
         )
     }
--- react-cli/src/index.js
+++ react-cli/src/index.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.css';
+import About from "./containers/about";
+import Inbox from "./containers/inbox";
+import Home from "./containers/home";
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+
+const routes = {
+    path: '/',
+    component: App,
+    childRoutes: [
+        { path: 'about', component: About },
+        { path: 'inbox', component: Inbox },
+        { path: 'home', component: Home }
+    ]
+}
+
+ReactDOM.render(  <Router>
+    <div>
+        <ul>
+            <li>
+                <Link to="/home">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/inbox">Inbox</Link>
+            </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/inbox" component={Inbox} />
+    </div>
+</Router>, document.getElementById('root'))
+
+//ReactDOM.render(<App />, document.getElementById('root'))
